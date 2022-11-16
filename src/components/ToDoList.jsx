900616fc204b1aceaa1dd1a3c99e5f3a5703ac1e
HoiLen <len1231.hoi@gmail.com>
2022-11-16 12:38:14 +0900

    renew

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -68,6 +68,9 @@ const ToDoList = () => {
             setCompleteList(tmpCompletedList)
             setList(tmpList)
         })
+        .catch((err) => {
+            console.log(err.status);
+        })
     };
 
     useEffect(() => {handleGetList()}, []);
